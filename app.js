0c71b98de0674a46c057dd89f008dff0f738172f
hobglobin27 <hobglobin27@gmail.com>
2019-01-25 00:33:31 -0600

    Prueba con mongo2

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const flash        = require('connect-flash')
 const SlackStrategy = require('passport-slack').Strategy
 
 mongoose
-  .connect(MONGODB, {useNewUrlParser: true})
+  .connect(process.env.MONGODB, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
