b1dace71a0fdd7f2ea1eb8be47cb143140b1f25d
hobglobin27 <hobglobin27@gmail.com>
2019-01-25 00:17:03 -0600

    Prueba con mongo

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const flash        = require('connect-flash')
 const SlackStrategy = require('passport-slack').Strategy
 
 mongoose
-  .connect("mongodb://hobglobin27:gopacha01@ds229771.mlab.com:29771/passport", {useNewUrlParser: true})
+  .connect(MONGODB, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
